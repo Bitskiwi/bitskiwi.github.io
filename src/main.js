c12fd737f6d40a99c0bb11fb61633fe9b8073c1b
Bit Kiwi <145420721+Bitskiwi@users.noreply.github.com>
2024-02-07 10:39:07 -0800

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+function alert_trap(prompt: string): void {
+    while(1){
+        if(alert(prompt)){
+            break;
+        } else {
+            continue;
+        }
+    }
+}
+
+var vim_trap = document.getElementById("vim_button");
+vim_trap.addEventListener("click", function() => {
+    alert_trap("Hey buddy! you should use Vim!");
+});

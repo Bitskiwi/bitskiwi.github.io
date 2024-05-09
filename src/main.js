9224fb756870755441fb724da335c02ed50e01e2
bitskiwi <varkiwibusiness@gmail.com>
2024-05-09 09:34:48 -0700

    this website sucks ill do it later

--- src/main.js
+++ src/main.js
@@ -10,5 +10,6 @@ function alert_trap(prompt: string): void {
 
 var vim_trap = document.getElementById("vim_button");
 vim_trap.addEventListener("click", function() => {
+	alert("works");
     alert_trap("Hey buddy! you should use Vim!");
 });
